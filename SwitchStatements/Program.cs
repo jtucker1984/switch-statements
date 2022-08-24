54aaa15793756e38e6704553a0e2708169570e9a
Jeremy Tucker <jeremytucker76@gmail.com>
2022-08-23 22:09:47 -0400

    switch case

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your favorite subject?");
+
+
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject)
+            {
+                case "biology":
+                    Console.WriteLine("biology is a hard one");
+                    break;
+             
+                case "science":
+                    Console.WriteLine("Science! THats a great subject.");
+                    break;
+                case "english":
+                    Console.WriteLine("English was cool I guess");
+                    break;
+                case "history":
+                    Console.WriteLine("History is fun");
+                    break;
+                case "math":
+                    Console.WriteLine("I hated math");
+                    break;
+                case "coding":
+                    Console.WriteLine("Really? I go to True Coders");
+                    break;
+                default:
+                    Console.WriteLine("you must be a genius");
+                    break;
+               
+
+
+        }    }
+            
+    }
+}
+
